2c53aa802706e3e1ef585b026099d51f6a5e6db2
Ben Stone <benjamin.h.stone@gmail.com>
2019-12-18 11:35:47 -0500

    initial commit

---
+++ PowerPlant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PowerPlant
+//
+//  Created by Benjamin Stone on 12/18/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
